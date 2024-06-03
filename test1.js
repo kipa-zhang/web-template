410719a7f64411c8ac22ef71cac2621c93f07e7a
KIPA <111>
2024-06-03 12:01:03 +0800

    1

--- test1.js
+++ test1.js
@@ -1,20 +1,5 @@
 // 
 function test() {
-    function test1 () {
-        function test2 () {
-            function test3 () {
-                function test4 () {
-                    function test5 () {
-                        function test6 () {
-                            function test7 () {
-                    
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
     for (let i = 0; i < 100; i++) {
         //  
         //  
